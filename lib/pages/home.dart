ae073bed235e8a1d1fdad2a73153c988da0a921d
Nayelipadron24 <nayelii.jaqui.p.v@gmail.com>
2024-10-22 13:42:21 -0600

    Primer commit

---
+++ lib/pages/home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(top: 50.0, left: 20.0, right: 10.0 ),
+        child: const Column(
+          children: [Text("Hello", style: TextStyle(color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.bold))],
+        )
+      ),
+    );
+  }
+}
